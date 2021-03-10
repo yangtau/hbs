a1fb228285c9fff94c3165aff50a52227e341b09
yangtau <32403743+yangtau@users.noreply.github.com>
2021-03-10 14:37:56 +0800

    test: unit tests for GetEndpoint

--- src/test/java/GetEndpointTest.java
+++ src/test/java/GetEndpointTest.java
@@ -9,6 +9,9 @@ import org.yangtau.hbs.hbase.coprocessor.GetEndpoint;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -30,8 +33,8 @@ class GetEndpointTest {
         assertFalse(rsp.getError(), rsp.getErrorMsg());
         assertTrue(rsp.hasValue());
         assertTrue(Arrays.equals(rsp.getValue().toByteArray(), expectValue));
-        assertEquals(rsp.getTimestamp(), expectVersion);
-        assertEquals(rsp.getCommitted(), expectCommitted);
+        assertEquals(expectVersion, rsp.getTimestamp());
+        assertEquals(expectCommitted, rsp.getCommitted());
     }
 
     private void expectGetEmpty(String row, long ts)
@@ -56,7 +59,7 @@ class GetEndpointTest {
     }
 
     // check if there is no read timestamp
-    private void expectNoReadTimestamp(String row, long version)
+    private void checkNoReadTimestamp(String row, long version)
             throws ExecutionException, InterruptedException, IOException {
         var res = TestUtil.get(conf, tableName, row, version, family);
         assertFalse(res.containsColumn(Bytes.toBytes(family), Constants.ReadTimestampQualifierBytes));
@@ -71,23 +74,164 @@ class GetEndpointTest {
 
         // Read uncommitted
         // get with a small ts, no RT should be written
-        TestUtil.put(conf, tableName, "row1", 1, family, Constants.DataQualifier, value);
-        TestUtil.put(conf, tableName, "row1", 1, family, Constants.CommitQualifier, "");
-        expectNoReadTimestamp("row1", 1);
+        TestUtil.put(conf, tableName, "row1", 1L, family, Constants.DataQualifier, value);
+        TestUtil.put(conf, tableName, "row1", 1L, family, Constants.CommitQualifier, "");
+        checkNoReadTimestamp("row1", 1L);
         // nothing should be read
-        expectGetEmpty("row1", 1);
-        expectNoReadTimestamp("row1", 1);
-        // expect write a (RT: 2) in version 1
-        expectGet("row1", 2, Bytes.toBytes(value), 1, false);
-        checkReadTimestamp("row1", 1, 2);
+        expectGetEmpty("row1", 1L);
+        checkNoReadTimestamp("row1", 1L);
+        // expect write a (RT: 2L) in version 1L
+        expectGet("row1", 2L, Bytes.toBytes(value), 1L, false);
+        checkReadTimestamp("row1", 1L, 2L);
 
         // Read committed
-        TestUtil.put(conf, tableName, "row2", 3, family, Constants.DataQualifier, value);
-        expectNoReadTimestamp("row2", 3);
-        // expect write a (RT: 4) in version 3
-        expectGet("row2", 4, Bytes.toBytes(value), 3, true);
-        checkReadTimestamp("row2", 3, 4);
+        TestUtil.put(conf, tableName, "row2", 3L, family, Constants.DataQualifier, value);
+        expectGetEmpty("row2", 3L);
+        checkNoReadTimestamp("row2", 3L);
+        // expect write a (RT: 4L) in version 3L
+        expectGet("row2", 4L, Bytes.toBytes(value), 3L, true);
+        checkReadTimestamp("row2", 3L, 4L);
 
         // TestUtil.DeleteTable(conf, tableName, family);
     }
+
+    @Test
+    void getFromMultiVersions() throws ExecutionException, InterruptedException, IOException {
+        TestUtil.DeleteTable(conf, tableName, family);
+        TestUtil.CreateTable(conf, tableName, family);
+
+        String row = "row100";
+        long length = 20;
+        Map<Long, byte[]> versionToValue = new HashMap<>();
+        var random = new Random();
+
+        for (long i = 0L; i < length; i += 3L) {
+            var ts = Math.abs(random.nextLong()) % length;
+            var value = random.nextDouble() + "";
+            TestUtil.put(conf, tableName, row, ts, family, Constants.DataQualifier, value);
+            versionToValue.put(ts, Bytes.toBytes(value));
+        }
+
+
+        for (long i = 0L; i < length + 1L; i++) {
+            final var ts = i;
+            var opt = versionToValue.entrySet().stream()
+                    .filter(e -> e.getKey() < ts)
+                    .max((e1, e2) -> (int) (e1.getKey() - e2.getKey()));
+
+            if (opt.isEmpty()) {
+                expectGetEmpty(row, ts);
+            } else {
+                expectGet(row, ts, opt.get().getValue(), opt.get().getKey(), true);
+            }
+        }
+    }
+
+    @Test
+    void multiGetOnTheSameData() throws ExecutionException, InterruptedException, IOException {
+        TestUtil.DeleteTable(conf, tableName, family);
+        TestUtil.CreateTable(conf, tableName, family);
+
+        // version 20
+        String row = "favorite programming language";
+        String value20 = "hedgehog";
+        long writeTs20 = 20L;
+        TestUtil.put(conf, tableName, row, writeTs20, family, Constants.DataQualifier, value20);
+
+        // version 15
+        String value15 = "lisp";
+        long writeTs15 = 15L;
+        TestUtil.put(conf, tableName, row, writeTs15, family, Constants.DataQualifier, value15);
+
+        checkNoReadTimestamp(row, writeTs15);
+        checkNoReadTimestamp(row, writeTs20);
+
+        // case 1: no RT before
+        {
+            // get version 15
+            expectGet(row, 20, Bytes.toBytes(value15), 15, true);
+            checkReadTimestamp(row, 15, 20);
+            // get version 20
+            expectGet(row, 30, Bytes.toBytes(value20), 20, true);
+            checkReadTimestamp(row, 20, 30);
+        }
+        // case 2: smaller RT than current one
+        // WT  RT
+        // 20  30
+        // 15  20
+        {
+            // get version 15
+            expectGet(row, 19, Bytes.toBytes(value15), 15, true);
+            checkReadTimestamp(row, 15, 20);
+            // get version 20
+            expectGet(row, 21, Bytes.toBytes(value20), 20, true);
+            checkReadTimestamp(row, 20, 30);
+        }
+        // case 3: bigger RT than current one
+        // WT  RT
+        // 20  30
+        // 15  20
+        {
+            // get version 20
+            expectGet(row, 31, Bytes.toBytes(value20), 20, true);
+            checkReadTimestamp(row, 20, 31);
+        }
+    }
+
+    @Test
+    void randomGet() throws ExecutionException, InterruptedException, IOException {
+        TestUtil.DeleteTable(conf, tableName, family);
+        TestUtil.CreateTable(conf, tableName, family);
+
+        String row = "row100";
+        long maxTs = 20;
+        Map<Long, byte[]> versionToValue = new HashMap<>();
+        Map<Long, Long> maxRts = new HashMap<>();
+        var random = new Random();
+
+        // prepared data in the `row`
+        for (long i = 0L; i < maxTs; i += 3L) {
+            var ts = Math.abs(random.nextLong()) % maxTs;
+            var value = random.nextDouble() + "";
+            TestUtil.put(conf, tableName, row, ts, family, Constants.DataQualifier, value);
+            versionToValue.put(ts, Bytes.toBytes(value));
+        }
+
+
+        for (long i = 0L; i < maxTs * 2; i++) {
+            var ts = Math.abs(random.nextLong()) % (maxTs * 2);
+
+            // the version will be read
+            var opt = versionToValue.entrySet().stream()
+                    .filter(e -> e.getKey() < ts)
+                    .max((e1, e2) -> (int) (e1.getKey() - e2.getKey()));
+            if (opt.isEmpty()) {
+                // nothing can be read, the ts is too small
+                expectGetEmpty(row, ts);
+            } else {
+                var writeTs = opt.get().getKey();
+                var value = opt.get().getValue();
+
+                // check RT before Get
+                if (!maxRts.containsKey(writeTs)) {
+                    // no read on this version before
+                    checkNoReadTimestamp(row, writeTs);
+                }
+
+                expectGet(row, ts, value, writeTs, true);
+                if (!maxRts.containsKey(writeTs)) {
+                    // no read on this version before
+                    maxRts.put(writeTs, ts);
+                    checkReadTimestamp(row, writeTs, ts);
+                } else {
+                    var preRt = maxRts.get(writeTs);
+                    var curRt = preRt > ts ? preRt : ts;
+                    maxRts.put(writeTs, curRt);
+                    checkReadTimestamp(row, writeTs, curRt);
+                }
+
+            }
+        }
+    }
+
 }
